a9902573d27ba29fdbec250da1ea2a8d29507428
Igor Ershov <gegobite@gmail.com>
2020-10-10 00:50:04 +0300

    Добавил удаление групп

--- unit-2/loginViewApp/SocialApp/GroupsTableViewController.swift
+++ unit-2/loginViewApp/SocialApp/GroupsTableViewController.swift
@@ -70,17 +70,16 @@ class GroupsTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            
+            dataSource.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
